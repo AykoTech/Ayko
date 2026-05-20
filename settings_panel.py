9f3e0de3a59970068a01d4deb9aab34f5526a625
Edoardo <pensi.edoardo@gmail.com>
2026-05-20 18:15:17 +0200

    Delete settings_panel.py

--- settings_panel.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
-
-class SettingsPanel(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("Settings"))
-        self.setLayout(layout)
